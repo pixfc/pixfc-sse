964327ecab34aedebc38e244666e4ead799ba987
pixelfc@gmail.com <pixelfc@gmail.com@1c64e79a-1f5e-ae51-cc15-efd862a25ce1>
2011-05-29 01:59:14 +0000

    trunk: tweak convert shell script to convert only 1 file

--- convert-yuyv-png.sh
+++ convert-yuyv-png.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-	echo "Usage: $0 <yuv_in_format> <width> <height>"
+if [ $# -ne 3 -a $# -ne 4 ]; then
+	echo "Usage: $0 <yuv_in_format> <width> <height> [filename]"
 	echo "where 'yuv_in_format' is either YUYV or UYVY"
 	echo "Input files must also bear that extension"
+	echo "If 'filename' is specified, onyl this file will be converted"
 	exit 1
 fi
 
@@ -11,6 +12,14 @@ ext=$1
 width=$2
 height=$3
 
+# Get list of files to convert
+if [ $# -eq 4 ]; then
+	filearray[0]="$4"
+else
+	readarray -t filearray < <(ls -1 *.${ext})
+fi
+
+# Set mplayer fileformat based on the provided extension
 if [ "${ext}" = "YUYV" ]; then
 	type="yuy2"
 elif [ "${ext}" = "UYVY" ]; then
@@ -20,7 +29,7 @@ else
 	exit 1
 fi
 
-readarray -t filearray < <(ls -1 *.${ext})
+# Perform conversion for each file
 for (( i=0; i<${#filearray[@]}; i++ ))
 do
 	echo ${filearray[$i]}
