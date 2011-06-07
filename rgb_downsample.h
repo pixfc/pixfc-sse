bec5d886d0cc53162f43eb88dfb304cd3d5eaf5b
pixelfc@gmail.com <pixelfc@gmail.com@1c64e79a-1f5e-ae51-cc15-efd862a25ce1>
2011-06-07 03:23:27 +0000

    trunk: fix bugs in RGB downsampling inlines

--- rgb_downsample.h
+++ rgb_downsample.h
@@ -291,13 +291,13 @@ EXTERN_INLINE void	avg_422_downsample_ag_rb_vectors_n_save_previous_sse2(__m128i
 	out_2_v16i_avg_422_ag_rb_vectors[0] = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[0], 0x8D);			//	PSHUFD		1	0.5
 	// A2 0		G2 0	A4 0	G4 0	A1 0	G1 0	A3 0	G3 0
 
-	_M(scratch1) = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[2], 0x8D);									//	PSHUFD		1	0.5
+	_M(scratch2) = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[2], 0x8D);									//	PSHUFD		1	0.5
 	// A6 0		G6 0	A8 0	G8 0	A5 0	G5 0	A7 0	G7 0
 
-	_M(scratch1) = _mm_unpacklo_epi64(out_2_v16i_avg_422_ag_rb_vectors[0], _M(scratch1));						// PUNPCKLQDQ	1	0.5
+	_M(scratch1) = _mm_unpacklo_epi64(out_2_v16i_avg_422_ag_rb_vectors[0], _M(scratch2));						// PUNPCKLQDQ	1	0.5
 	// A2 0		G2 0	A4 0	G4 0	A6 0	G6 0	A8 0	G8 0
 
-	out_2_v16i_avg_422_ag_rb_vectors[0] = _mm_unpackhi_epi64(out_2_v16i_avg_422_ag_rb_vectors[0], _M(scratch1));// PUNPCKHQDQ	1	0.5
+	out_2_v16i_avg_422_ag_rb_vectors[0] = _mm_unpackhi_epi64(out_2_v16i_avg_422_ag_rb_vectors[0], _M(scratch2));// PUNPCKHQDQ	1	0.5
 	// A1 0		G1 0	A3 0	G3 0	A5 0	G5 0	A7 0	G7 0
 
 	_M(scratch2) = _mm_srli_si128(in_2_v16i_previous_ag_rb_vectors[0], 12);										// PSRLDQ		1	0.5
@@ -327,13 +327,13 @@ EXTERN_INLINE void	avg_422_downsample_ag_rb_vectors_n_save_previous_sse2(__m128i
 	out_2_v16i_avg_422_ag_rb_vectors[1] = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[1], 0x8D);			//	PSHUFD		1	0.5
 	// R2 0		B2 0	R4 0	B4 0	R1 0	B1 0	R3 0	B3 0
 
-	_M(scratch1) = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[3], 0x8D);									//	PSHUFD		1	0.5
+	_M(scratch2) = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[3], 0x8D);									//	PSHUFD		1	0.5
 	// R6 0		B6 0	R8 0	B8 0	R5 0	B5 0	R7 0	B7 0
 
-	_M(scratch1) = _mm_unpacklo_epi64(out_2_v16i_avg_422_ag_rb_vectors[1], _M(scratch1));						// PUNPCKLQDQ	1	0.5
+	_M(scratch1) = _mm_unpacklo_epi64(out_2_v16i_avg_422_ag_rb_vectors[1], _M(scratch2));						// PUNPCKLQDQ	1	0.5
 	// R2 0		B2 0	R4 0	B4 0	R6 0	B6 0	R8 0	B8 0
 
-	out_2_v16i_avg_422_ag_rb_vectors[1] = _mm_unpackhi_epi64(out_2_v16i_avg_422_ag_rb_vectors[1], _M(scratch1));// PUNPCKHQDQ	1	0.5
+	out_2_v16i_avg_422_ag_rb_vectors[1] = _mm_unpackhi_epi64(out_2_v16i_avg_422_ag_rb_vectors[1], _M(scratch2));// PUNPCKHQDQ	1	0.5
 	// R1 0		B1 0	R3 0	B3 0	R5 0	B5 0	R7 0	B7 0
 
 	_M(scratch2) = _mm_srli_si128(in_2_v16i_previous_ag_rb_vectors[1], 12);										// PSRLDQ		1	0.5
@@ -930,7 +930,7 @@ EXTERN_INLINE void	avg_422_downsample_first_r_g_b_vectors_sse2_ssse3(__m128i* in
  * R12 0	B12 0	R34 0	B34 0	R56 0	B56 0	R78 0	B78 0
  */
 EXTERN_INLINE void	avg_422_downsample_first_ag_rb_vectors_sse2(__m128i* in_4_v16i_current_ag_rb_vectors, __m128i *out_2_v16i_avg_422_ag_rb_vectors) {
-	CONST_M128I(keep_1st, 0x000000000000FFFFLL, 0x0000000000000000LL);
+	CONST_M128I(keep_1st, 0x00000000FFFFFFFFLL, 0x0000000000000000LL);
 	M128I(scratch1, 0x0LL, 0x0LL);
 	M128I(scratch2, 0x0LL, 0x0LL);
 	M128I(scratch3, 0x0LL, 0x0LL);
@@ -944,13 +944,13 @@ EXTERN_INLINE void	avg_422_downsample_first_ag_rb_vectors_sse2(__m128i* in_4_v16
 	out_2_v16i_avg_422_ag_rb_vectors[0] = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[0], 0x8D);			//	PSHUFD		1	0.5
 	// A2 0		G2 0	A4 0	G4 0	A1 0	G1 0	A3 0	G3 0
 
-	_M(scratch1) = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[2], 0x8D);									//	PSHUFD		1	0.5
+	_M(scratch2) = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[2], 0x8D);									//	PSHUFD		1	0.5
 	// A6 0		G6 0	A8 0	G8 0	A5 0	G5 0	A7 0	G7 0
 
-	_M(scratch1) = _mm_unpacklo_epi64(out_2_v16i_avg_422_ag_rb_vectors[0], _M(scratch1));						// PUNPCKLQDQ	1	0.5
+	_M(scratch1) = _mm_unpacklo_epi64(out_2_v16i_avg_422_ag_rb_vectors[0], _M(scratch2));						// PUNPCKLQDQ	1	0.5
 	// A2 0		G2 0	A4 0	G4 0	A6 0	G6 0	A8 0	G8 0
 
-	out_2_v16i_avg_422_ag_rb_vectors[0] = _mm_unpackhi_epi64(out_2_v16i_avg_422_ag_rb_vectors[0], _M(scratch1));// PUNPCKHQDQ	1	0.5
+	out_2_v16i_avg_422_ag_rb_vectors[0] = _mm_unpackhi_epi64(out_2_v16i_avg_422_ag_rb_vectors[0], _M(scratch2));// PUNPCKHQDQ	1	0.5
 	// A1 0		G1 0	A3 0	G3 0	A5 0	G5 0	A7 0	G7 0
 
 	_M(scratch2) = _mm_and_si128(in_4_v16i_current_ag_rb_vectors[0], _M(keep_1st));								// PAND			1	0.5
@@ -974,13 +974,13 @@ EXTERN_INLINE void	avg_422_downsample_first_ag_rb_vectors_sse2(__m128i* in_4_v16
 	out_2_v16i_avg_422_ag_rb_vectors[1] = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[1], 0x8D);			//	PSHUFD		1	0.5
 	// R2 0		B2 0	R4 0	B4 0	R1 0	B1 0	R3 0	B3 0
 
-	_M(scratch1) = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[3], 0x8D);									//	PSHUFD		1	0.5
+	_M(scratch2) = _mm_shuffle_epi32(in_4_v16i_current_ag_rb_vectors[3], 0x8D);									//	PSHUFD		1	0.5
 	// R6 0		B6 0	R8 0	B8 0	R5 0	B5 0	R7 0	B7 0
 
-	_M(scratch1) = _mm_unpacklo_epi64(out_2_v16i_avg_422_ag_rb_vectors[1], _M(scratch1));						// PUNPCKLQDQ	1	0.5
+	_M(scratch1) = _mm_unpacklo_epi64(out_2_v16i_avg_422_ag_rb_vectors[1], _M(scratch2));						// PUNPCKLQDQ	1	0.5
 	// R2 0		B2 0	R4 0	B4 0	R6 0	B6 0	R8 0	B8 0
 
-	out_2_v16i_avg_422_ag_rb_vectors[1] = _mm_unpackhi_epi64(out_2_v16i_avg_422_ag_rb_vectors[1], _M(scratch1));// PUNPCKHQDQ	1	0.5
+	out_2_v16i_avg_422_ag_rb_vectors[1] = _mm_unpackhi_epi64(out_2_v16i_avg_422_ag_rb_vectors[1], _M(scratch2));// PUNPCKHQDQ	1	0.5
 	// R1 0		B1 0	R3 0	B3 0	R5 0	B5 0	R7 0	B7 0
 
 	_M(scratch2) = _mm_and_si128(in_4_v16i_current_ag_rb_vectors[1], _M(keep_1st));								// PAND			1	0.5
