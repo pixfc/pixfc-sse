142502f51b26a0952b4a3254b30b3273e718e0e2
pixelfc@gmail.com <pixelfc@gmail.com@1c64e79a-1f5e-ae51-cc15-efd862a25ce1>
2011-06-10 01:22:52 +0000

    trunk: added bash script to count to occurences of a given assembly instruction in a function

---
+++ count_instr.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 1 -a $# -ne 2 ]; then
+    echo "$0 <symbol name> [instruction]"
+	echo "If [instruction] is not provided, 'movdqa' is assumed'"
+    exit 1
+fi
+
+if [ ! -f ./unit-testing ]; then
+    echo "./unit-testing not in current dir"
+    exit 1
+fi
+
+instr=${2:-movdqa}
+
+# Check OS and set the disassemble command and sed pattern
+echo $OSTYPE | grep -q "linux"
+if [ $? -eq 0 ]; then
+    cmd='objdump -d'
+    pattern="/<$1>/,/^$/ p"
+else # assume Mac
+    cmd='otool -tV'
+    pattern="/$1:/,/^_/ p"
+fi
+
+${cmd} unit-testing | sed -n "${pattern}" | grep -i "${instr}" | wc -l
+
+
