b5a2ee6fd05d2fdb13e917a038f019200ddc4340
pixelfc@gmail.com <pixelfc@gmail.com@1c64e79a-1f5e-ae51-cc15-efd862a25ce1>
2011-10-01 03:51:53 +0000

    trunk: fix convert-to-png script so it checks its arguments

--- tools/convert-yuyv-png.sh
+++ tools/convert-yuyv-png.sh
@@ -1,20 +1,22 @@
 #!/bin/bash -x
 
-#if [ $# -ne 3 -a $# -ne 4 ]; then
-#	echo "Usage: $0 <yuv_in_format> <width> <height> [filename]"
-#	echo "where 'yuv_in_format' is either YUYV, UYVY or YUV422p"
-#	echo "Input files must also bear that extension"
-#	echo "If 'filename' is specified, onyl this file will be converted"
-#	exit 1
-#fi
+if [ $# -ne 1 -a $# -ne 2 ]; then
+	echo "Usage: $0 <yuv_in_format> [filename]"
+	echo "where 'yuv_in_format' is either YUYV, UYVY or YUV422p"
+	echo "Input files must also bear that extension"
+	echo "If 'filename' is specified, onyl this file will be converted"
+	exit 1
+fi
 
 ext=$1
-width=$2
-height=$3
 
 # Get list of files to convert
-if [ $# -eq 4 ]; then
-	filearray[0]="$4"
+if [ $# -eq 2 ]; then
+	if [ ! -f "$2" ]; then
+		echo "File \'$2\' does not exist"
+		exit 1
+	fi
+	filearray[0]="$2"
 else
 	readarray -t filearray < <(ls -1 *.${ext})
 fi
@@ -29,7 +31,10 @@ if [ "${ext}" = "YUYV" ]; then
 elif [ "${ext}" = "UYVY" ]; then
 	cmd="mplayer -demuxer rawvideo -rawvideo format=uyvy:w=WIDTH:h=HEIGHT -vo png:z=0"
 elif [ "${ext}" = "YUV422p" ]; then
-	# We need to convert to YUYV first, using one of our tools.
+	# I cant get mplayer's rawvideo module to accept the 422p format,
+	# so we do the conversion in 2 steps: to yuyv first using one of our tool
+	# then to png using mplayer
+	
 	# Ensure yuv422p_to_yuyv exists, otherwise build it
 	if [ ! -x "$(dirname $0)/yuv422p_to_yuyv" ]; then
 		gcc "$(dirname $0)/yuv422p_to_yuyv.c" -o "$(dirname $0)/yuv422p_to_yuyv"
@@ -51,6 +56,11 @@ do
 	width=${name%%_*}
 	rest="${name#*_}"
 	height=${rest%%_*}
+
+	if [ -z "$width" -o -z "$height" ]; then
+		echo "Invalid width or height in filename: " $name
+		exit 1
+	fi
 
 	# replace WIDTH and HEIGHT with actual value in commands
 	cmd="${cmd/WIDTH/$width}"
